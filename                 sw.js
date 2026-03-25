519ab5c30358beb9b1e02af5f8165edfa306e9ff
01joaotaboca <163590842+01joaotaboca@users.noreply.github.com>
2026-03-25 18:57:34 -0300

    Add files via upload

---
+++                 sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'agropro-v5-cache';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './agropro-logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
